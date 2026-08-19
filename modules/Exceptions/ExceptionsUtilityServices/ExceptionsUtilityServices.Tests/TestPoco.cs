b3703080e0485d2e2bd67ab9bc7e31f0829aeb24
jayhuang820 <jay.huang@solventosoft.com.tw>
2026-08-19 18:15:42 +0800

    fix: re-register submodules properly in git index

--- modules/Exceptions/ExceptionsUtilityServices/ExceptionsUtilityServices.Tests/TestPoco.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ExceptionsUtilityServices.Tests;
-
-public class TestPoco
-{
-    [Required(AllowEmptyStrings = true, ErrorMessage = "Username is required.")]
-    public string Name { get; set; } = string.Empty;
-}
